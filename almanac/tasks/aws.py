b45a64749b426b3ff8f57a21b845fc1b402336c1
Tyler Fisher <tfisher@politico.com>
2018-02-13 17:51:06 -0500

    aws task

--- almanac/tasks/aws.py
+++ almanac/tasks/aws.py
@@ -13,26 +13,46 @@ OUTPUT_PATH = 'election-results/{0}/calendar/'
 
 
 @shared_task(acks_late=True)
-def serialize_calendar(cycle):
+def serialize_calendar(cycle, division):
     data = []
 
     events = ElectionEvent.objects.filter(
         election_day__cycle__name=cycle
-    )
+    ).exclude(
+        event_type=ElectionEvent.GENERAL
+    ).order_by('election_day__date', 'division__label')
     for event in events:
         serialized = ElectionEventSerializer(event)
         data.append(serialized.data)
 
-    json_string = JSONRenderer().render(data)
-    publish_to_aws(json_string, cycle)
-
-
-def publish_to_aws(data, cycle):
     key = os.path.join(
         OUTPUT_PATH.format(cycle),
         'data.json'
     )
+    json_string = JSONRenderer().render(data)
+    publish_to_aws(json_string, key)
 
+    state_data = []
+    state_events = ElectionEvent.objects.filter(
+        division=division,
+        election_day__cycle__name=cycle
+    ).order_by('election_day__date', 'division__label')
+
+    for event in state_events:
+        serialized = ElectionEventSerializer(event)
+        state_data.append(serialized.data)
+
+    state_key = os.path.join(
+        OUTPUT_PATH.format('{0}/{1}'.format(
+            cycle, division.slug
+        )),
+        'data.json'
+    )
+    state_json_string = JSONRenderer().render(state_data)
+    publish_to_aws(state_json_string, state_key)
+
+
+def publish_to_aws(data, key):
     bucket = get_bucket()
 
     bucket.put_object(
